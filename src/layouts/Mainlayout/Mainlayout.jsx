a2795ff98d96bde183c003e92c7a534dd3511c9c
hossenrabiul <robiulhossen0081@gmail.com>
2025-09-28 19:45:49 +0600

    solved error

--- src/layouts/Mainlayout/Mainlayout.jsx
+++ src/layouts/Mainlayout/Mainlayout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Header } from "../../pages/Header/Header";
 import About from "../../pages/About/About";
 import './Mainlayout.css'
-import Works from "../../pages/works/Works";
+import Works from '../../pages/Works/Works'
 import Projects from "../../pages/Projects/Projects";
 import Testimonials from "../../pages/Testimonials/Testimonials";
 import { Contact } from "../../pages/Contact/Contact";
